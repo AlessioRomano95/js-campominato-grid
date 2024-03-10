cd9915cd6ed107a1e7ccb67a0dd20960ff8e73ff
AlessioRomano95 <romanalex400@gmail.com>
2024-03-10 07:22:26 +0100

    [ADD] header e start js

--- script.js
+++ script.js
@@ -1,1 +1,12 @@
 console.log('js ok')
+
+// 1. Selezione degli elementi HTML
+// 2. Impostazioni di base per la griglia di gioco
+// 3. Calcolo del numero totale di celle
+// 4. Funzione per creare una cella della griglia
+// 5. Event listener per il click sul bottone "inizia gioco"
+// 7. Event listener per il click sulla cella
+// 8. Aggiunta della cella alla griglia nel documento HTML
+
+//1.
+const button = document.getElementById('pulsante')
