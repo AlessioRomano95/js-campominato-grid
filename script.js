f72a9bd4f4d1c6256e7ebbb610706e8cd8548691
AlessioRomano95 <romanalex400@gmail.com>
2024-03-10 08:23:05 +0100

    [ADD] js e grid

--- script.js
+++ script.js
@@ -8,5 +8,37 @@ console.log('js ok')
 // 7. Event listener per il click sulla cella
 // 8. Aggiunta della cella alla griglia nel documento HTML
 
-//1.
+// 1.
 const button = document.getElementById('pulsante')
+const grid = document.getElementById('grid')
+
+// 2.
+const rows = 10
+const cols = 10 
+
+// 3.
+const totCells = rows * cols 
+
+// 4.
+const createCells = content => {
+    const cells = document.createElement('div')
+    cells.className = 'cells'
+    cells.innerText = content
+    return cells
+}
+
+// 5.
+button.addEventListener('click', function(){
+    for (let i = 1; i <= totCells; i++){
+        const newCells = createCells(i)
+        
+        // 7.
+        newCells.addEventListener('click', () => {
+            newCells.classList.toggle('clicca')
+            console.log(newCells)
+        })
+
+        // 8.
+        grid.appendChild(newCells)
+    }
+})
